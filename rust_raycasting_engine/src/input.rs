0eebd6e8b7754b9e21a3c11cb3964f2f006ff28c
Heros <leh8ca@bosch.com>
2026-08-13 15:37:03 -0300

    refactored the engine to the repo root

--- rust_raycasting_engine/src/input.rs
+++
@@ -1,9 +0,0 @@
-pub struct InputState {
-    pub last_mouse_x: Option<f32>,
-}
-
-impl InputState {
-    pub fn new() -> Self {
-        Self { last_mouse_x: None }
-    }
-}
--- rust_raycasting_engine/src/main.rs
+++
@@ -1,6 +0,0 @@
-use rre::run_game;
-
-// for testing purposes
-fn main() {
-    run_game("assets/config.ron");
-}
