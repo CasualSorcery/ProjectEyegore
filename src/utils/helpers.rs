76eca0f6ff7e0a2a4c9eda8e1129aa450082ae4a
Heros <herosleal4@gmail.com>
2026-08-18 15:38:07 -0300

    refactored the engine window manager, dished minifb for winit and softbuffer. major changes to input handling and such, but overall rendering logic untouched.

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -1,8 +1,6 @@
-use crate::core::config::GameConfig;
 use image::{DynamicImage, RgbaImage};
-use minifb::{Window, WindowOptions};
 
-// loads/parses a single 64x64 png texture, returns the texture u32 vector
+// loads a single 64x64 png texture, returns the texture u32 vector
 pub fn load_texture(filepath: &str) -> Vec<u32> {
     // tries to open the image file (png only), panics if can't
     let img: DynamicImage = image::open(filepath)
@@ -19,38 +17,14 @@ pub fn load_texture(filepath: &str) -> Vec<u32> {
 
     // loops through each pixel, unpacks them to the rgba value, and appends each to the texture buffer
     for (x, y, pixel) in rgba_image.enumerate_pixels() {
-        let r: u32 = pixel[0] as u32;
-        let g: u32 = pixel[1] as u32;
-        let b: u32 = pixel[2] as u32;
-        // TODO: add alpha to RGBA calculation
+        let r: u32 = pixel.0[0] as u32;
+        let g: u32 = pixel.0[1] as u32;
+        let b: u32 = pixel.0[2] as u32;
+        let _a: u32 = pixel.0[3] as u32;
 
-        // minifb uses 0x00RRGGBB (ignoring alpha) *for now*
-        let color: u32 = (r << 16) + (g << 8) + b;
+        let color: u32 = (r << 16) | (g << 8) | b;
         texture_buffer[(y as usize) * 64 + (x as usize)] = color;
     }
     // returns the image as an u32 vector of RGB pixels
     texture_buffer
-}
-
-// create a window using the params specifications
-pub fn create_window(options: &GameConfig) -> Window {
-    // creates a window object using minifb
-    let mut window_options = WindowOptions::default();
-
-    let mut window: Window = Window::new(
-        &options.name, // window name only accepts &str
-        options.scr_width,
-        options.scr_height,
-        window_options,
-    )
-        .unwrap_or_else(|e| {
-            // "work or panic" basically
-            panic!("Failed to create window: {}", e);
-        });
-
-    // sets the fps limit & target for application
-    window.set_target_fps(options.target_fps);
-
-    // returns the window object
-    window
 }
